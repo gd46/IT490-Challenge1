fc5524f9e45a080ee09c1212fea2c26d170ca9a0
Gd46 <gd46@njit.edu>
2014-10-01 21:03:06 -0400

    Added checkColumnheadings function

---
+++ fileHandling.php
@@ -0,0 +1,18 @@
+<?php
+	class fileHandling {
+	
+		public function checkColumnheadings($handle, $hasColumnheadings){
+			
+			while(($row = fgetcsv($handle, ",")) !== FALSE){
+					if($hasColumnheadings){
+						$column_heading = $row;
+						$hasColumnheadings = FALSE;
+					}
+					else{
+						$record = array_combine($column_heading, $row);
+						$records[] = $record;
+					}
+				}
+				return $records;
+		}
+?>
