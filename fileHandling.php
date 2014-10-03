1df522c2706cf98d076cb0e331fe8b7dcb9055f9
Gd46 <gd46@njit.edu>
2014-10-02 23:31:22 -0400

    Added getHeadings function

--- fileHandling.php
+++ fileHandling.php
@@ -17,5 +17,23 @@
 				}
 				return $records;
 		}
+		
+		public static function getHeadings($handle, $hasColumnheadings){
+			$records = [];
+			while(($row = fgetcsv($handle)) !== FALSE){
+					
+					if($hasColumnheadings){
+						$column_heading = $row;
+						$hasColumnheadings = FALSE;
+					}
+					else{
+						$record = array_combine($column_heading, $row);
+						$records[$record['varname']] = $record['varTitle'];
+					}	
+					
+				}
+				
+				return $records;
+		}
 	}
 ?>
