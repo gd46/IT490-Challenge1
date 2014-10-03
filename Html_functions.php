bc0280d43ea908ebda143eb6e66c70871cda3b64
Gd46 <gd46@njit.edu>
2014-10-02 23:33:54 -0400

    Print the human readable headings

--- Html_functions.php
+++ Html_functions.php
@@ -1,12 +1,15 @@
 <?php
 	
+	
 	class Html_functions{
 		
 		public static function printTable($records,$url_var){
 			if(isset($_GET[$url_var])){
+				$file2 = uploadFile::fileUpload('hd2013_varlist.csv');
+				$headings = fileHandling::getHeadings($file2,TRUE);
 				echo '<table border="1">';
 				foreach($records[$_GET[$url_var]] as $key => $value){
-					echo '<tr><th>' . $key . '</th>';
+					echo '<tr><th>' . $headings[$key] . '</th>';
 					echo '<td>' . $value . '</td></tr>';
 				}
 				echo '</table>';
