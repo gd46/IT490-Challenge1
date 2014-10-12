42dfe360411d8b69e189ecd3206e1cbafe8d6c68
Giuseppe DiBella <giuseppedb@Giuseppes-MacBook-Pro-2.local>
2014-10-12 18:30:49 -0400

    Altered printTable function

--- Html_functions.php
+++ Html_functions.php
@@ -4,16 +4,17 @@
 	class Html_functions{
 		
 		//Function for printing a table by passing in an array, and passing in one url variable. 
-		public static function printTable($records,$url_var){
-			if(isset($_GET[$url_var])){
-				$file2 = uploadFile::fileUpload('hd2013_varlist.csv');
-				$headings = fileHandling::getHeadings($file2,TRUE);
-				echo '<table border="1">';
-				foreach($records[$_GET[$url_var]] as $key => $value){
-					echo '<tr><th>' . $headings[$key] . '</th>';
-					echo '<td>' . $value . '</td></tr>';
+		public static function printVerticaltable($records,$headings){
+			if(isset($_GET['record'])){
+				$table = '<div id="verticalTable"><table border="1">';
+				$i = 0;
+				foreach($records[$_GET['record'] as $key => $value){
+					$table .= '<tr><th>' . $headings[$i]['varTitle'] . '</th>';
+					$table .= '<td>' . $value . '</td></tr>';
+					$i++;
 				}
-				echo '</table>';
+				$table .= '</table></div>';
+				echo $table;
 			}
 		}
 	}
