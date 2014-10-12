3ef4f2b8dec74f88e00cf5f5c8cf1ccb42c3c986
Giuseppe DiBella <giuseppedb@Giuseppes-MacBook-Pro-2.local>
2014-10-12 18:38:24 -0400

    Cleaned up the print links function

--- printLinks.php
+++ printLinks.php
@@ -1,24 +1,18 @@
 <?php
 	
-	require 'Html_functions.php';
-	
 	class printLinks {
 		
 		//Function gets called when a new printLinks object is created. 
 		//This function prints links based of of the array it is passed. 
 		//Calls the printTable function to print the data for the record clicked. 
-		public function __construct($records){
-			
+		public function __construct($records, $headings){
 			$i = -1;
 			if(empty($_GET)){
 				foreach($records as $record){
 					$i++;
-					echo '<a href="?record=' .$i. '">' . $record['INSTNM'] . '</a>';
-					echo'</p>';
+					Html_functions::makeLink('record',$i, $record['INSTNM']);
 				}
 			}
-				
-			Html_functions::printTable($records, 'record');
 		}
 	}
 ?>
