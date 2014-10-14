6b1875d0c456351a837011aea0a0e2a28dcc5a25
Giuseppe DiBella <giuseppedb@Giuseppes-MacBook-Pro-2.local>
2014-10-13 21:51:24 -0400

    Added the fileHandling file to the proper directory

---
+++ Classes/File/fileHandling.php
@@ -0,0 +1,24 @@
+<?php
+
+	namespace Classes\File;
+	
+	class fileHandling {
+		
+		
+		public function checkColumnheadings($handle, $hasColumnheadings){
+			
+			while(($row = fgetcsv($handle, ",")) !== FALSE){
+					if($hasColumnheadings){
+						$column_heading = $row;
+						$hasColumnheadings = FALSE;
+					}
+					else{
+						$record = array_combine($column_heading, $row);
+						$records[] = $record;
+					}
+				}
+				\Classes\File\manageFile::closeFile($handle);
+				return $records;
+		}
+	}
+?>
